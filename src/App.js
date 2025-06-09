fad39ea8ca480bf22966af6d3e4084ca62231823
johann.silang <johann.silang@gmail.com>
2025-06-09 16:30:34 +0800

    s70-silang

--- src/App.js
+++ src/App.js
@@ -5,14 +5,54 @@ import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
+import AppNavbar from "./components/AppNavbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+
 function App() {
+  const [user, setUser] = useState({
+    id: null,
+    isAdmin: null
+  });
+
+  function unsetUser(){
+    localStorage.clear();
+  };
+
+  useEffect(() => {
+    fetch("https://myr5j4i3c2.execute-api.us-west-2.amazonaws.com/production/users/details", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data && data._id && data.isAdmin){
+        setUser({
+          id: data._id,
+          isAdmin: data.isAdmin
+        });
+      }else{
+        setUser({
+          id: null,
+          isAdmin: null
+        });
+      }
+    })
+  }, [])
+
   return (
-    <UserProvider>
+    <UserProvider value={{user, setUser, unsetUser}}>
       <Router>
-        {/*<AppNavbar />*/}
+        <AppNavbar />
         <Container>
           <Routes>
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
           </Routes>
         </Container>
       </Router>
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,32 @@
+import {useContext} from "react";
+import UserContext from "../UserContext";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
+export default function AppNavbar() {
+  const {user} = useContext(UserContext);
+  return (
+    <Navbar expand="lg" className="bg-primary">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" exact="true">Home</Nav.Link>
+            {
+              user.id
+              ?
+                <Nav.Link as={NavLink} to="/logout" exact="true">Logout</Nav.Link>
+              :
+                <>
+                <Nav.Link as={NavLink} to="/login" exact="true">Login</Nav.Link>
+                <Nav.Link as={NavLink} to="/register" exact="true">Register</Nav.Link>
+                </>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
