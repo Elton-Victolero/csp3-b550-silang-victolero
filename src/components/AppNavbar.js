3fdd37dd41a9951cf3e6411a2808a2f4522dd37a
victolero.elton <victolero.elton@gmail.com>
2025-06-11 14:39:50 +0800

    Added OrdersAdminView

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -23,6 +23,7 @@ export default function AppNavbar() {
                 ?
                   <>
                     <Nav.Link as={NavLink} to="/dashboard" className="text-white">Dashboard</Nav.Link>
+                    <Nav.Link as={NavLink} to="/orders" className="text-white">Orders</Nav.Link>
                     <Nav.Link as={NavLink} to="/logout" className="text-white">Logout</Nav.Link>
                   </>
                 :
